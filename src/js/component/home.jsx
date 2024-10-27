d065ca7832402f9767529dfd4546d56aa004426b
Angel Gallego <angelgb1990@gmail.com>
2024-10-27 12:39:06 +0000

    funcion completa

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import "/src/styles/index.css"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setcolor] = useState("amarillo")
 	return (
-		<div className="text-center">
-			<div></div>
-			<div></div>
-			<div></div>
+		<>
+		<div className="container">
+			<div className="semaforo">
+				<div onClick={() => setcolor("rojo")} className={"rojo" + (color === "rojo" ? "foco" : "")}>1</div>
+				<div onClick={() => setcolor("amarillo")} className={"amarillo" + (color === "amarillo" ? "foco" : "")} >2</div>
+				<div onClick={() => setcolor("verde")} className={"verde" + (color === "verde" ? "foco" : "")} >3</div>
+			</div>
 		</div>
+			<div className="p-4 d-flex justify-content-center">
+				<button>1</button>
+				<button>2</button>
+			</div>
+		</>
 	);
 };
 
