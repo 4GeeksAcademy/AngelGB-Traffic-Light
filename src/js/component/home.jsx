b6de8e01f65040fdba5dfe4b8bdc3a6d58447430
Angel Gallego <angelgb1990@gmail.com>
2024-11-06 12:26:13 +0000

    arreglos finales

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,88 @@
 import React from "react";
 import { useState } from "react";
 import "/src/styles/index.css"
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Color from "./Color";
 
 //create your first component
 const Home = () => {
-	const [color, setcolor] = useState("amarillo")
+	const [color, setColor] = useState("amarillo");
+	const [extraColor, setExtraColor] = useState("extra")
+	const [intervalo, setIntervalo] = useState(null);
+	const [intervaloExtra, setIntervaloExtra] = useState(null);
+	const [nuevo, setNuevo] = useState(false)
+
+	const cambioColor = () => {
+		setColor((prevColor) => {
+			if (prevColor === "rojo") return "verde";
+			else if (prevColor === "amarillo") return "rojo";
+			else return "amarillo";
+		});
+	};
+	
+	const cambioColorExtra = () => {
+		setExtraColor((prevColorExtra) => {
+			return prevColorExtra === "extra" ? "extrafoco" : "extra";
+		});
+	};
+
+	const semaforoAutomatico = () => {
+		if (!intervalo) {
+			const intervaloGen = setInterval(cambioColor , 2000);
+			setIntervalo(intervaloGen);
+		}
+	};
+	const semaforoExtra = () => {
+		if (!intervaloExtra) {
+			const intervaloGen = setInterval(cambioColorExtra , 1000);
+			setIntervaloExtra(intervaloGen);
+		}
+	};
+
+	const automaticosJuntos = () => {
+			semaforoAutomatico(),
+			semaforoExtra()
+	}
+
+	const pararAutomatico = () => {
+		if (intervalo) {
+			clearInterval(intervalo);
+			setIntervalo(null);
+		}
+	};
+
+	const pararExtra = () => {
+		if (intervaloExtra) {
+			clearInterval(intervaloExtra);
+			setIntervaloExtra(null);
+		}
+	};
+
+	const pararJuntos = () => {
+			pararAutomatico()
+			pararExtra()
+	}
+
+	const nuevoColor = () => {
+		setNuevo(prev => !prev)
+	}
+
 	return (
 		<>
-		<div className="container">
-			<div className="semaforo">
-				<div onClick={() => setcolor("rojo")} className={"rojo" + (color === "rojo" ? "foco" : "")}>1</div>
-				<div onClick={() => setcolor("amarillo")} className={"amarillo" + (color === "amarillo" ? "foco" : "")} >2</div>
-				<div onClick={() => setcolor("verde")} className={"verde" + (color === "verde" ? "foco" : "")} >3</div>
+		<div className="p-4"><h1 className="text-center">Semaforo automatizado</h1></div>
+			<div className="container pt-5">
+				<div className="semaforo">
+					<div onClick={() => setColor("rojo")} className={"rojo" + (color === "rojo" ? "foco" : "")}></div>
+					<div onClick={() => setColor("amarillo")} className={"amarillo" + (color === "amarillo" ? "foco" : "")} ></div>
+					<div onClick={() => setColor("verde")} className={"verde" + (color === "verde" ? "foco" : "")} ></div>
+					<div>
+						{nuevo == true ? <Color nombreClase={extraColor} /> : ""}
+					</div>
+				</div>
 			</div>
-		</div>
 			<div className="p-4 d-flex justify-content-center">
-				<button>1</button>
-				<button>2</button>
+				<button onClick={automaticosJuntos} >Automatico</button>
+				<button onClick={pararJuntos} >Parar</button>
+				<button onClick={nuevoColor} >Extra</button>
 			</div>
 		</>
 	);
